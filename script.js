f5802a8b95e9637bf07ec83c11d638e4a8570d42
dobrostanandrij109 <dobrostanandrij109@gmail.com>
2024-12-22 17:12:47 +0200

    abubA

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-let cartProd = document.getElementById('cart-products');
-let cart = []
 let productsGrid = document.getElementById('products-grid');
 let productsArray = [];
-let product = productsArray.find(function(p)){
-    return p.id == id;
-}
 let xhr = new XMLHttpRequest();
-let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
+let url = 'https://minishop-e5dd.restdb.io/rest';
 xhr.open('GET',url + '/products');
+xhr.setRequestHeader("content-type", "application/json");
+xhr.setRequestHeader("x-apikey", "67682326e70533a0cd3584e9");
+xhr.setRequestHeader("cache-control", "no-cache");
 xhr.responseType = 'json'
 xhr.onload = function() {
     productsArray = xhr.response
@@ -21,20 +19,53 @@ xhr.onload = function() {
             <img class='product-photo' src='${p.photo_url}' alt='${p.name}'>
             <p class='product-price'><b>Price: </b>${p.price}$</p>
             <p class='product-description'><b>Description: </b>${p.description}</p>
-            <a href='userProfile.html?id=${p.author_id}'>Seller profile</a>
-            <button onclick="addProductToCart(${p.id})">Buy</button>
+            <button onclick="addProductToCart('${p._id}')">Buy</button>
         `;
         productsGrid.append(pElem);
     });
 }
 xhr.send();
-function openCart(){
+// CART ----------------
+let cartProd = document.getElementById('cart-products');
+let cart = [];
+if(localStorage.getItem('cart')) {
+    cart = JSON.parse(localStorage.getItem('cart'));
+    drawCartProducts();
+}
+function addProductToCart(id) {
+    let product = productsArray.find(function(p) {
+        return p._id == id;
+    })
+    cart.push(product);
+    drawCartProducts();
+    localStorage.setItem("cart", JSON.stringify(cart));
+    document.getElementById('cart-button').classList.add('active');
+    setTimeout(function(){
+        document.getElementById('cart-button').classList.remove('active');
+    },500);
+}
+function drawCartProducts() {
+    if(cart.length === 0) return cartProd.innerHTML = 'Cart is empty';
+    cartProd.innerHTML = null;
+    let sum = 0;
+    cart.forEach(function(p){
+        cartProd.innerHTML += `
+            <p><img src="${p.photo_url}"> ${p.name} |${p.price}$</p>
+            <hr>
+        `;
+        sum += p.price;
+    });
+    cartProd.innerHTML += `
+        <p>Total Price: ${sum}$</p>
+        <button onclick="buyAll()">Buy All</button>
+    `;
+}
+function buyAll() {
+    cart = [];
+    cartProd.innerHTML = 'Money was withdrawn from your credit card';
+    localStorage.setItem("cart", '[]');
+}
+function openCart() {
     cartProd.classList.toggle('hide');
 }
 
-function addProductToCart (id) { 
-    let product = productsArray.find(function(p)){
-        return p.id == id;
-    }
-    cart.push(product)
- }
